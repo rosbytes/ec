38fbbe92ca81561d222634001571380e87c79aba
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-24 06:16:38 +0530

    feat: enhance tRPC setup with middleware for logging and rate limiting

--- api/src/trpc/appRouter.ts
+++ api/src/trpc/appRouter.ts
@@ -5,6 +5,7 @@ import { z } from "zod"
 // Define a simple router
 export const appRouter = router({
     user: userRouter,
+    // initial setup testing procedure/endpoint
     greeting: publicProcedure.input(z.object({ name: z.string() })).query(({ input }) => {
         return `Hello, ${input.name}! Welcome to tRPC`
     }),
--- api/src/trpc/index.ts
+++ api/src/trpc/index.ts
@@ -1,4 +1,4 @@
-export { router, publicProcedure } from "./trpc"
+export { router, publicProcedure, userProcedure, vendorProcedure, t } from "./trpc"
 
 // appRouter exports
 export { appRouter } from "./appRouter"
