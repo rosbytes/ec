e4c0adadb9e4159745447497e2688cbd496d9370
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-24 06:29:08 +0530

    feat: update user schema to include first and last name fields and add verified status

--- api/src/db/schema/users.ts
+++ api/src/db/schema/users.ts
@@ -1,16 +1,19 @@
 import { relations } from "drizzle-orm"
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core"
+import { pgTable, uuid, varchar, boolean } from "drizzle-orm/pg-core"
 import { timestamps } from "../columnHelper"
 import { userAddresses } from "./userAddresses"
 import { orders } from "./orders"
 
 export const users = pgTable("users", {
     id: uuid().primaryKey().defaultRandom(),
-    name: varchar({ length: 255 }).notNull(),
+    // first name and last name, last could be null
+    firstName: varchar("first_name", { length: 255 }).notNull(),
+    lastName: varchar("last_name", { length: 255 }),
 
     // phone length, ideally should be 15
     phone: varchar({ length: 20 }).notNull().unique(),
     email: varchar({ length: 255 }),
+    verified: boolean().default(false).notNull(),
     ...timestamps,
 })
 
