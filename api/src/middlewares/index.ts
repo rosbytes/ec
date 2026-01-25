ae9deaad698dca50a918e142c27414ebc870b4ee
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-25 07:43:21 +0530

    feat: enhance middleware by adding context types for user and vendor

--- api/src/middlewares/index.ts
+++ api/src/middlewares/index.ts
@@ -1,2 +1,7 @@
+// user middleware and user context exports
 export { isUser } from "./user.middleware"
+export type { UserContext } from "./user.middleware"
+
+// vendor middleware and vendor context exports
 export { isVendor } from "./vendor.middleware"
+export type { VendorContext } from "./vendor.middleware"
