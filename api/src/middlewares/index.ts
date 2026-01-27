652f009d0a51a284c49d1f50e91467032c0bc019
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-27 23:26:20 +0530

    Add board authentication middleware and update middleware exports

--- api/src/middlewares/index.ts
+++ api/src/middlewares/index.ts
@@ -5,3 +5,7 @@ export type { UserContext } from "./user.middleware"
 // vendor middleware and vendor context exports
 export { isVendor } from "./vendor.middleware"
 export type { VendorContext } from "./vendor.middleware"
+
+// board middleware and board context exports
+export { isBoard } from "./board.middleware"
+export type { BoardContext } from "./board.middleware"
