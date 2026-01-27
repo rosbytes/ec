d345a6dd5489e5b65a93a5ebe42b86a0bf78ee37
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-27 23:23:56 +0530

    Add inventory management features with nearby vendor lookup

--- api/src/modules/user/index.ts
+++ api/src/modules/user/index.ts
@@ -1,3 +1,4 @@
 export { userAuthRouter } from "./user.routes"
 export { userAddressRouter } from "./address.routes"
 export { cartRouter } from "./cart.routes"
+export { userInventoryRouter } from "./inventory.routes"
